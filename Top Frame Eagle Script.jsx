4e4f8ac6f04c613cd7ce8640febd5d4166f33f5c
Daly <dalidaliovic@gmail.com>
2024-10-17 04:32:49 +0100

    Top Frame Eagle Script

---
+++ Top Frame Eagle Script.jsx
@@ -0,0 +1,63 @@
+// Top Frame Eagle Script
+
+// Create the main panel
+var mainPanel = new Window("palette", "Top Frame Eagle", undefined);
+mainPanel.orientation = "column";
+
+// Create input field for the name
+var nameGroup = mainPanel.add("group");
+nameGroup.orientation = "row";
+nameGroup.add("statictext", undefined, "Enter Name:");
+var nameInput = nameGroup.add("edittext", undefined, "");
+nameInput.characters = 20;
+
+// Create button to apply changes
+var applyButton = mainPanel.add("button", undefined, "Apply Changes");
+
+// Function to change composition name and update text layer
+function applyChanges() {
+    var enteredName = nameInput.text;
+    
+    // Get the active composition
+    var activeComp = app.project.activeItem;
+    
+    if (activeComp && activeComp instanceof CompItem) {
+        // Change the name of the active composition
+        activeComp.name = "Top Frame Eagle " + enteredName;
+        
+        // Find the "Name Placeholder" composition
+        var namePlaceholderComp;
+        for (var i = 1; i <= activeComp.numLayers; i++) {
+            var layer = activeComp.layer(i);
+            if (layer.source instanceof CompItem && layer.source.name === "Name Placeholder") {
+                namePlaceholderComp = layer.source;
+                break;
+            }
+        }
+        
+        if (namePlaceholderComp) {
+            // Change the text of the last layer in the "Name Placeholder" composition
+            var lastLayer = namePlaceholderComp.layer(namePlaceholderComp.numLayers);
+            if (lastLayer instanceof TextLayer) {
+                var textProp = lastLayer.property("Source Text");
+                var textDocument = textProp.value;
+                textDocument.text = enteredName;
+                textProp.setValue(textDocument);
+            } else {
+                alert("The last layer in 'Name Placeholder' composition is not a text layer.");
+            }
+        } else {
+            alert("Could not find 'Name Placeholder' composition in the active composition.");
+        }
+        
+        alert("Changes applied successfully!");
+    } else {
+        alert("Please select a composition first.");
+    }
+}
+
+// Attach the function to the button
+applyButton.onClick = applyChanges;
+
+// Show the panel
+mainPanel.show();
